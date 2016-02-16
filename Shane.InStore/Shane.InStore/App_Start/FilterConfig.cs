a98f74acc9b6341498bcecbcd4b28ca8c2f9a6c5
shanevdbroek@gmail.com <Technology101>
2016-02-16 19:49:12 +0200

    first project

---
+++ Shane.InStore/Shane.InStore/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Shane.InStore
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
