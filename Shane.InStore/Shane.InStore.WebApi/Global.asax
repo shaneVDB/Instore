4b88ab1e02daeb7acb57fb1422b40874dfa02cc3
ShaneV <shanevdbroek@gmail.com>
2016-02-23 11:13:08 +0200

    WebApi Project
    Added Employee model

---
+++ Shane.InStore/Shane.InStore.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shane.InStore.WebApi.WebApiApplication" Language="C#" %>
---
+++ Shane.InStore/Shane.InStore.WebApi/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shane.InStore.WebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Branch { get; set; }
+
+        [Required]
+        [StringLength(6, ErrorMessage = "Employee Number too long.")]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+        
+    }
+}
